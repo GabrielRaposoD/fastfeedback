76dadc2b0f7e22db97ebd84123e815b66c62ee30
Gabriel Raposo <gabriel.robertor@gmail.com>
2020-09-23 21:25:36 -0300

    Added auth with gitHub

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,24 @@
-import React from 'react'
-
-import '../css/tailwind.css'
-import '../css/main.css'
-import GoogleFonts from 'next-google-fonts'
-import { DefaultSeo } from 'next-seo'
+import React from 'react';
+import '../css/tailwind.css';
+import '../css/main.css';
+import GoogleFonts from 'next-google-fonts';
+import { DefaultSeo } from 'next-seo';
+import { ProvideAuth } from '../lib/auth';
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <DefaultSeo
         titleTemplate={
-          router.route === '/' ? 'Exsign Studio' : '%s | Exsign Studio'
+          router.route === '/' ? 'Fast Feed Back' : '%s | Fast Feed Back'
         }
       />
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" />
-      <Component {...pageProps} />
+      <GoogleFonts href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap' />
+      <ProvideAuth>
+        <Component {...pageProps} />
+      </ProvideAuth>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
-import { NextSeo } from 'next-seo'
+import { NextSeo } from 'next-seo';
+import { auth } from 'firebase';
+import { useAuth } from '../lib/auth';
 
 export default function Index() {
+  const auth = useAuth();
+
   return (
     <>
       <NextSeo
-        title="Exsign Studio"
-        description="Uma equipe de designers e desenvolvedores de ponta com o objetivo de trazer a experiência e design que seus usuários merecem."
+        title='Fast Feed Back'
+        description='Uma equipe de designers e desenvolvedores de ponta com o objetivo de trazer a experiência e design que seus usuários merecem.'
       />
-      <div className="flex items-center justify-center min-h-screen overflow-hidden">
-        <h1 className="text-4xl font-semibold">Hello World</h1>
+      <div className='flex flex-col items-center justify-center min-h-screen overflow-hidden text-black'>
+        <h1 className='text-4xl font-semibold'>Hello World</h1>
+        <div className='flex flex-row'>
+          <button onClick={(e) => auth.signinWithGitHub()}>Sign-in</button>
+          {auth?.user && (
+            <button className='ml-4' onClick={(e) => auth.signout()}>
+              Sign-out
+            </button>
+          )}
+        </div>
+        <p>{auth?.user?.email}</p>
       </div>
     </>
-  )
+  );
 }
